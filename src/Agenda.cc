22cf1b89d53fa5a5664b86794faf43c4143a6c07
Pau Argelaguet <pau@outlook.com>
2015-05-03 13:11:08 +0200

    Afegides funcions que faltaven

--- src/Agenda.cc
+++ src/Agenda.cc
@@ -1,52 +1,68 @@
 #include "Agenda.hh"
 
 Agenda::Agenda(string d, string h) {
-  // TODO: Hem de convertir els strings en un objecte instant i guardar-lo a r
-  //this->r = r;
+	// TODO: Hem de convertir els strings en un objecte instant i guardar-lo a r
+  	//this->r = r;
 }
 
 void Agenda::modificar_rellotge(const Comanda &c) {
-  if (c.nombre_dates() == 1)
-    r.modificar_data(c.data(1));
-  if (c.te_hora())
-    r.modificar_hora(c.hora);
+	/*
+  	if (c.nombre_dates() == 1)
+		r.modificar_data(c.data(1));
+  	if (c.te_hora())
+		r.modificar_hora(c.hora);
+	*/
 }
 
 void Agenda::inserir_tasca(const Comanda &c) {
   
 }
 
+void Agenda::modificar_tasca(const Comanda &c){
+
+}
+
+void Agenda::esborrar_tasca(const Comanda &c){
+
+}
+
 void Agenda::interpretar_modificar_rellotge(const Comanda &c) {
-    Instant nou(consultar_rellotge());
-    if (c.nombre_dates() == 1)
-        nou.modificar_data(c.data(1));
-    if (c.te_hora())
-        nou.modificar_hora(c.hora());
-    if (not(nou < consultar_rellotge()))
-      // TODO: Gestionar quan és igual, que no canviï
-        modificar_rellotge(nou);
-    else
-        cout << ERROR << endl;
+	/*
+	Instant nou(consultar_rellotge());
+	if (c.nombre_dates() == 1)
+		nou.modificar_data(c.data(1));
+	if (c.te_hora())
+		nou.modificar_hora(c.hora());
+	if (not(nou < consultar_rellotge()))
+	  	// TODO: Gestionar quan és igual, que no canviï
+		modificar_rellotge(nou);
+	else
+		cout << ERROR << endl;
+	*/
 }
 
 void Agenda::interpretar_inserir_tasca(const Comanda &c) {
-    vector<string> etiquetes(c.nombre_etiquetes());
-    for (int i = 1; i <= c.nombre_etiquetes(); ++i)
-        etiquetes[i-1] = c.etiqueta(i);
-    if(c.nombre_dates() == 0){
-      // TODO: Posar data actual
-    }
-    inserir_tasca(c.titol(), Instant(c.data(1), c.hora), etiquetes);
+	/*
+	vector<string> etiquetes(c.nombre_etiquetes());
+	for (int i = 1; i <= c.nombre_etiquetes(); ++i)
+		etiquetes[i-1] = c.etiqueta(i);
+	if(c.nombre_dates() == 0){
+	  	// TODO: Posar data actual
+	}
+	inserir_tasca(c.titol(), Instant(c.data(1), c.hora), etiquetes);
+	*/
 }    
 
 void Agenda::interpretar_escriure_tasques(const Comanda &c) {
-    string etiquetes;
-    if (c.nombre_etiquetes() == 1)
-        etiquetes = c.etiqueta(1);
-    else if (c.te_expressio())
-        etiquetes = c.expressio();
-    else
-        etiquetes = "";
+	/*
+	string etiquetes;
+	if (c.nombre_etiquetes() == 1)
+		etiquetes = c.etiqueta(1);
+	else if (c.te_expressio())
+		etiquetes = c.expressio();
+	else
+		etiquetes = "";
+	*/
 }    
    
 void Agenda::interpretar_modificar_tasca(const Comanda &c){
@@ -56,3 +72,27 @@ void Agenda::interpretar_modificar_tasca(const Comanda &c){
 void Agenda::interpretar_esborrar_tasca(const Comanda &c){
 
 };
+
+void Agenda::interpretar_esborrar_etiqueta(const Comanda &c){
+
+}
+
+void Agenda::interpretar_esborrar_etiquetes(const Comanda &c){
+
+}
+
+Instant Agenda::consultar_rellotge(){
+	return r;
+}
+
+void Agenda::escriure_rellotge(){
+
+}
+
+void Agenda::escriure_tasques_futures(const Comanda &c){
+
+}
+
+void escriure_tasques_passades(){
+	
+}
--- src/Agenda.hh
+++ src/Agenda.hh
@@ -12,73 +12,73 @@ using namespace std;
 
 class Agenda {
 
-  private:
+  	private:
 
-      Instant r;
+		Instant r;
     
-  public:
+  	public:
     
-      //Constructors
-      
-      //Pre: cert
-      //Post: es crea una agenda buida amb rellotge 'r'
-      Agenda(string d, string h);
-      
-      //Modificadors
-      
-      //Pre: cert
-      //Post: el valor del rellotge del parametre implicit passa a ser el contingut de la comanda
-      void modificar_rellotge(const Comanda &c);
-      
-      //Pre: cert
-      //Post: s'afegeix una tasca espicificada a la comanda
-      void inserir_tasca(const Comanda &c);
-      
-      //Pre: s'ha realitzat una consulta de tasques futures
-      //Post: modifica la tasca espicificada o retorna error si aquesta no existeix
-      void modificar_tasca(const Comanda &c);
-      
-      //Pre: s'ha realitzat una consulta de tasques
-      //Post: s'ha esborrat la tasca especificada o retorna error si aquesta no existeix
-      void esborrar_tasca();
-      
-      
-      // TODO: Potser retornar un bool si la funcio sha executat OK?
-      
-      void interpretar_modificar_rellotge(const Comanda &c);
-      
-      void interpretar_inserir_tasca(const Comanda &c);
-      
-      void interpretar_escriure_tasques(const Comanda &c);
-      
-      void interpretar_modificar_tasca(const Comanda &c);
+		//Constructors
+		
+		//Pre: cert
+		//Post: es crea una agenda buida amb rellotge 'r'
+		Agenda(string d, string h);
+		
+		//Modificadors
+		
+		//Pre: cert
+		//Post: el valor del rellotge del parametre implicit passa a ser el contingut de la comanda
+		void modificar_rellotge(const Comanda &c);
+		
+		//Pre: cert
+		//Post: s'afegeix una tasca espicificada a la comanda
+		void inserir_tasca(const Comanda &c);
+		
+		//Pre: s'ha realitzat una consulta de tasques futures
+		//Post: modifica la tasca espicificada o retorna error si aquesta no existeix
+		void modificar_tasca(const Comanda &c);
+		
+		//Pre: s'ha realitzat una consulta de tasques
+		//Post: s'ha esborrat la tasca especificada o retorna error si aquesta no existeix
+		void esborrar_tasca(const Comanda &c);
+		
+		
+		// TODO: Potser retornar un bool si la funcio sha executat OK?
+		
+		void interpretar_modificar_rellotge(const Comanda &c);
+		
+		void interpretar_inserir_tasca(const Comanda &c);
+		
+		void interpretar_escriure_tasques(const Comanda &c);
+		
+		void interpretar_modificar_tasca(const Comanda &c);
 
-      void interpretar_esborrar_tasca(const Comanda &c);
-      
-      void interpretar_esborrar_etiqueta(const Comanda &c);
-      
-      void interpretar_esborrar_etiquetes(const Comanda &c);
-      
-      //Consultores
-      //Pre: 
-      //Post:
-      Instant consultar_rellotge();
-      
-      //Lectura
-      
-      //Escriptura
-      
-      //Pre: cert
-      //Post: s'ha escrit el rellotge al canal de sortida
-      void escriure_rellotge();
-      
-      //Pre: cert
-      //Post: s'han escrit les tasques especificades al canal de sortida
-      void escriure_tasques_futures(const Comanda &c);
-      
-      //Pre: cert
-      //Post: s'han escrit totes les tasques passades al canal de sortida
-      void escriure_tasques_passades();
+		void interpretar_esborrar_tasca(const Comanda &c);
+		
+		void interpretar_esborrar_etiqueta(const Comanda &c);
+		
+		void interpretar_esborrar_etiquetes(const Comanda &c);
+		
+		//Consultores
+		//Pre: 
+		//Post:
+		Instant consultar_rellotge();
+		
+		//Lectura
+		
+		//Escriptura
+		
+		//Pre: cert
+		//Post: s'ha escrit el rellotge al canal de sortida
+		void escriure_rellotge();
+		
+		//Pre: cert
+		//Post: s'han escrit les tasques especificades al canal de sortida
+		void escriure_tasques_futures(const Comanda &c);
+		
+		//Pre: cert
+		//Post: s'han escrit totes les tasques passades al canal de sortida
+		void escriure_tasques_passades();
     
 };
 
