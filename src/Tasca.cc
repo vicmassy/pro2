48af85153c615eef650fca23f34510bdc755fc84
Pau Argelaguet <pau@outlook.com>
2015-05-05 13:22:17 +0200

    Afegides tasques al cc

--- src/Tasca.cc
+++ src/Tasca.cc
@@ -8,6 +8,22 @@ Tasca::Tasca(string n, map<string,bool> &e){
 
 }
 
+void Tasca::afegir_etiqueta(string tag){
+
+}
+
+void Tasca::esborrar_etiqueta(string tag){
+
+}
+
+void Tasca::esborrar_totes_etiquetes(){
+
+}
+
+bool Tasca::te_etiqueta(string tag){
+	return true;
+}
+
 void Tasca::escriure_tasca(){
-	
+
 }
